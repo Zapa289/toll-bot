2a5d886fb7adb625acfee5a70de00725e694831d
Jack <74797201+Zapa289@users.noreply.github.com>
2022-03-21 09:21:22 -0500

    Rework user (#2)
    * Removed UserManager and moved functionality into TollBot.
    
    * Fix ValueError calls. Add a stub for datepicker change so that the slack UI doesn't show an error. Need to fix the dates going from slack to db.
    
    * Add a new UserDate class for keeping the dates in so conversion between database and display format is behind the scenes.
    
    * Overhaul User to use the date class for managing dates instead of complicated strings.
    
    * Convert list of strings from db into list of dates for the User. Also, can't have an Overflow menu with only 1 option. Will perhaps use another UI element in the future.
    
    Co-authored-by: JLittle <jack.tay.little@hpe.com>

--- bot_entry.py
+++ bot_entry.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_sdk import WebClient
-from datetime import datetime
+from datetime import date, datetime
 
 app = Flask(__name__)
 
@@ -19,6 +19,14 @@ def publish_home_tab(client: WebClient, user_id):
     home_tab = bot.home_tab(user_id)
     client.views_publish(user_id=user_id, view=home_tab)
 
+def get_selected_value(payload):
+    """Returns the selected option from an overflow menu"""
+    return payload["selected_option"]["value"]
+
+def get_view_block_list(body):
+    """Returns list of blocks in current view"""
+    return body['view']['blocks']
+
 @bolt_app.event('app_home_opened')
 def app_home_opened(client: WebClient, event, logger):
     """User clicked the home tab"""
@@ -28,16 +36,17 @@ def app_home_opened(client: WebClient, event, logger):
 
 @bolt_app.action('addDate')
 def add_date(ack, client: WebClient, context, body):
-
     ack()
 
-    # Find the current state of the datepicker
-    date_raw = body['view']['state']['values']['DateBlock']['DatePicker']['selected_date']
+    # Find the current state of the datepicker. Location of the datepicker
+    # is as laid out in home.make_home_blocks()
+    current_state = body['view']['state']['values']
+    input_date = current_state['DateBlock']['DatePicker']['selected_date']
     # None means datepicker has been left as default, use today's date
-    date = date_raw if date_raw else datetime.today().strftime(settings.RAW_DATE_FORMAT)
+    new_date = date.fromisoformat(input_date) if input_date else date.today()
 
     user_id = context['user_id']
-    bot.add_date(user_id, date)
+    bot.add_user_date(user_id, new_date)
     publish_home_tab(client, user_id)
 
 @bolt_app.action(re.compile('date_menu_\d'))
@@ -46,12 +55,19 @@ def date_menu(ack, client, payload, body, context):
 
     user_id = context['user_id']
 
-    if payload["selected_option"]["value"] == "remove_date":
-        for block in body['view']['blocks']:
+    if  get_selected_value(payload) == "remove_date":
+        for block in get_view_block_list(body):
             if block['block_id'] == payload['block_id']:
-                date = block['text']['text']
-        bot.delete_date(user_id, date)
+                raw_date = block['text']['text']
+
+        selected_date = datetime.strptime(raw_date, settings.DATE_FORMAT).date()
+        
+        bot.delete_user_date(user_id, selected_date)
         publish_home_tab(client, user_id)
+
+@bolt_app.action("DatePicker")
+def date_picker(ack):
+    ack()
 
 handler = SlackRequestHandler(bolt_app)
 
--- lib/user.py
+++ lib/user.py
@@ -1,26 +1,10 @@
-from xmlrpc.client import DateTime
-import settings
-from db_manager import DatabaseAccess
-from datetime import datetime
-
-def valid_date(date: str) -> bool:
-    """Verifies date strings are correct format"""
-
-    try:
-        datetime.strptime(date, settings.RAW_DATE_FORMAT)
-    except (ValueError, TypeError):
-        print(f"Invalid date \"{date}\", check date formatting")
-        return False
-    return True
-
-def get_date_from_str(date: str) -> DateTime:
-    return datetime.strptime(date, settings.RAW_DATE_FORMAT)
-
+from datetime import date
+    
 class User:
     """Contains Slack user information"""
     def __init__(self, user_id) -> None:
         self.id = user_id
-        self._dates: list[str] = []
+        self._dates: list[date] = []
 
     @property
     def id(self):
@@ -28,7 +12,7 @@ class User:
 
     @id.setter
     def id(self, user_id: str):
-        if user_id[0] != "U":
+        if user_id[0] != "U" or not user_id[1:].isalnum():
             raise ValueError("Not a valid User ID!")
         self._id = user_id
 
@@ -37,52 +21,21 @@ class User:
         return self._dates
 
     @dates.setter
-    def dates(self, date_list: list[str]) -> list[str]:
-        for date in date_list:
-            if not valid_date(date):
-                raise ValueError(date=date)
-        self._dates = sorted(date_list, key=get_date_from_str)
+    def dates(self, date_list: list[date]):
+        self._dates = sorted(date_list)
 
-    def add_date(self, date: str):
-        if not valid_date(date):
-            raise ValueError(date=date)
-        self._dates.append(date)
+    def add_date(self, new_date: date):
+        if new_date in self._dates:
+            print("Date already tracked by user")
+            raise ValueError
+        self._dates.append(new_date)
 
-    def delete_date(self, date: str):
-        self._dates.remove(date)
+    def delete_date(self, del_date: date):
+        try:
+            self._dates.remove(del_date)
+        except ValueError as e:
+            print(f"Date not found in user: {del_date}")
+            raise e
 
     def __repr__(self):
-        return f"User ID {self.id}, dates: {self.dates}"
-
-class UserManager:
-    def __init__(self, db: DatabaseAccess) -> None:
-        self.db = db
-
-    def get_user(self, user_id) -> User:
-        user = User(user_id)
-        try:
-            user.dates = self.db.get_user_dates(user.id)
-        except ValueError as e:
-            print(f"Invalid date seen in database for user \"{user.id}\": {e['date']}")
-        return user
-
-    def add_date(self, user: User, date: str) -> User:
-        try:
-            user.add_date(date)
-        except ValueError:
-            print(f"Unable to add date \"{date}\" to user \"{user.id}\"")
-            return user
-        # Only modify the database if we are working with a valid date
-        self.db.add_date(user.id, date)
-        return user
-
-    def delete_date(self, user: User, date: str) -> User:
-        try:
-            user.delete_date(date)
-        except ValueError:
-            print(f"Unable to delete date \"{date}\" from user \"{user.id}\"")
-            return user
-
-        # Only modify the database if we are working with a valid date
-        self.db.delete_date(user.id, date)
-        return user
+        return f"User ID {self.id}\n\tDates: {self.dates}"
