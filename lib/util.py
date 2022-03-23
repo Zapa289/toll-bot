9c4ce7684add10a88d2e0316134270a5da6a7c42
JLittle <jack.tay.little@hpe.com>
2022-03-23 10:49:24 -0500

    Check that the cache directory exists before writing.

--- lib/util.py
+++ lib/util.py
@@ -1,10 +1,14 @@
-import settings
+from settings import IMAGE_CACHE_PATH
 from hashlib import md5
+from os import mkdir
 
 def hash_user_id(user_id: str) -> str:
     return md5(user_id.encode('utf-8')).hexdigest()
 
 def write_to_image_cache(filename: str, datastream):
-    with open(settings.IMAGE_CACHE_PATH/filename, "wb") as file:
+    if not IMAGE_CACHE_PATH.exists():
+        mkdir(IMAGE_CACHE_PATH)
+
+    with open(IMAGE_CACHE_PATH/filename, "wb") as file:
         for chunk in datastream:
             file.write(chunk)
