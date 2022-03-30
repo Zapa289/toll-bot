cfb61027e759c69c618965314a96f20182faa46b
Jack <74797201+Zapa289@users.noreply.github.com>
2022-03-30 15:24:09 -0500

    Get rid of lib directory, no real point. Redo some google stuff. Put the directions from Google Maps into a Directions class which extends the necessary info as attributes. Change up file_util; if there is more than one image belonging to a user in the image cache all but the newest will be deleted. (#10)
    Co-authored-by: JLittle <jack.tay.little@hpe.com>

--- google.py
+++ google.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from settings import GOOGLE_MAPS_API_KEY, base_logger, IMAGE_SIZE
 from offices import get_office
 from googlemaps import Client
@@ -5,48 +6,62 @@ from googlemaps.maps import StaticMapMarker
 
 logger = base_logger.getChild(__name__)
 
-def get_poly_from_route(directions) -> str:
-    return directions[0]['overview_polyline']['points']
+@dataclass
+class Directions:
+    directions: dict
 
-def get_markers_from_directions(directions) -> list[dict]:
-    """Returns a list of lat/lng dicts of the start and end of the route."""
-    leg = directions[0]['legs'][0]
-    start_marker = leg["start_location"]
-    end_marker = leg["end_location"]
+    @property
+    def _leg(self):
+        return self.directions[0]['legs'][0]
 
-    return [start_marker, end_marker]
+    @property
+    def markers(self) -> list[str]:
+        start_marker = self._leg["start_location"]
+        end_marker = self._leg["end_location"]
 
+        return [start_marker, end_marker]
+
+    @property
+    def start_address(self):
+        return self._leg['start_address']
+
+    @property
+    def end_address(self):
+        return self._leg['end_address']
+
+    @property
+    def polyline(self):
+        return self.directions[0]['overview_polyline']['points']
 
 class Mapper:
     def __init__(self):
         self.key = GOOGLE_MAPS_API_KEY
         self.client = Client(key=self.key)
 
-    def get_route(self, location:str, office_str: str):
+    def get_route(self, location:str, office_str: str) -> Directions:
+        """Get Google Maps route between the given location and chosen office"""
         logger.info("Creating route")
         office = get_office(office_str)
         if not office:
             logger.error(f"Office {office_str} not found")
             raise ValueError("Invalid office input")
 
-        return self.client.directions(location, f"place_id:{office.place_id}")
+        return Directions(self.client.directions(location, f"place_id:{office.place_id}"))
 
-    def get_static_map(self, directions, toll_coords: list=None):
+    def get_static_map(self, directions: Directions, toll_coords: list=None):
         """Get a static map from google. Returns a generator to get picture contents"""
         logger.info("Get static map")
-        # Get polyline from route
-        route_polyline = get_poly_from_route(directions)
 
         markers = []
-        markers.append(StaticMapMarker(get_markers_from_directions(directions)))
+        markers.append(StaticMapMarker(directions.markers))
         if toll_coords:
             markers.append(StaticMapMarker(toll_coords))
 
-        logger.debug(f"\tPolyline: {route_polyline}\n\tMarkers: {markers}")
+        logger.debug(f"Polyline: {directions.polyline}")
 
         # Generate static map from polyline
         try:
-            datastream = self.client.static_map(size=IMAGE_SIZE, path="enc:" + route_polyline, markers=markers)
+            datastream = self.client.static_map(size=IMAGE_SIZE, path="enc:" + directions.polyline, markers=markers)
         except ValueError:
             logger.error("Failed to get static map", exc_info=True)
             return None
